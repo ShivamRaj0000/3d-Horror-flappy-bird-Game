9a57960c1a1baad8625f204044fa9136c1dc65ea
ShivamRaj0000 <161716782+ShivamRaj0000@users.noreply.github.com>
2026-05-17 15:37:42 +0530

    Power Up

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float speed = 5f;
+    public float coinRotateSpeed = 60f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right*coinRotateSpeed *Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (gameObject.CompareTag("PowerUp"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
